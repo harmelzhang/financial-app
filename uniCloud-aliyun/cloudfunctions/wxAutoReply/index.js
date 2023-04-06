61dccb44cf94c92dd65cc611e39c855d174fe2fd
zhangpeng <harmel@163.com>
2023-04-06 15:03:21 +0800

    自动回复主营业务

--- uniCloud-aliyun/cloudfunctions/wxAutoReply/index.js
+++ uniCloud-aliyun/cloudfunctions/wxAutoReply/index.js
@@ -24,7 +24,12 @@ exports.main = async (event, context) => {
 
 	if(msgType === "text") {
 		var result = `找不到您要查询的信息，请检查是否输入正确`
-	    return `<xml><ToUserName><![CDATA[${fromUser}]]></ToUserName><FromUserName><![CDATA[${toUser}]]></FromUserName><CreateTime>${createTime}</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[${result}]]></Content></xml>`
+		const dbJQL = uniCloud.databaseForJQL({event, context})
+		const queryResp = await dbJQL.collection("stock").where(`stock_name=='${msg}'`).limit(1).get()
+		if(queryResp.affectedDocs != 0) {
+			result = queryResp.data[0].main_business
+		}
+		return `<xml><ToUserName><![CDATA[${fromUser}]]></ToUserName><FromUserName><![CDATA[${toUser}]]></FromUserName><CreateTime>${createTime}</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[${result}]]></Content></xml>`
 	}
 
 	return "success"
