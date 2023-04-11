d7196c1f1394ed9a53988567376b66b8e433ba7a
zhangpeng <harmel@163.com>
2023-04-11 17:07:16 +0800

    增加返回指数信息

--- uniCloud-aliyun/cloudfunctions/queryStockByCode/index.js
+++ uniCloud-aliyun/cloudfunctions/queryStockByCode/index.js
@@ -4,11 +4,21 @@ exports.main = async (event, context) => {
 
 	const code = event.code || event.queryStringParameters.code
 	const dbJQL = uniCloud.databaseForJQL({event, context})
-	const queryResp = await dbJQL.collection("stock").where(`code=='${code}'`).limit(1).get()
-	if(queryResp.affectedDocs != 0) {
+
+	var queryResp = await dbJQL.collection("stock").where(`code=='${code}'`).limit(1).get()
+	if(queryResp.code == 0 && queryResp.affectedDocs != 0) {
 		result.success = true
 		result.data = queryResp.data[0]
 	}
+	
+	queryResp = await dbJQL.collection("index_sample").where(`stock_code=='${code}'`).orderBy("type_code").get()
+	if(queryResp.code == 0 && queryResp.affectedDocs != 0) {
+		var indexs = []
+		queryResp.data.forEach(function(item) {
+			indexs.push(item.type_name)
+		})
+	}
+	result.data.index = indexs
 
 	return result
 };
