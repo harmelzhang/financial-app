ee18f7af2954aa9c2a93dcd39404b960b64f0f15
zhangpeng <harmel@163.com>
2023-04-18 19:07:28 +0800

    查询用户股票标记信息

---
+++ uniCloud-aliyun/cloudfunctions/queryUserMarkedStocks/index.js
@@ -0,0 +1,20 @@
+'use strict';
+exports.main = async (event, context) => {
+	var result = {"success": false, "data": []}
+
+	const openid = event.openid || event.queryStringParameters.openid
+	const dbJQL = uniCloud.databaseForJQL({event, context})
+
+	var queryResp = await dbJQL.collection("stock_favorites").where(`openid=='${openid}'`).orderBy("create_time desc").get()
+	
+	console.log(queryResp)
+	
+	if(queryResp.code == 0 && queryResp.affectedDocs != 0) {
+		result.success = true
+		queryResp.data.forEach(function(item) {
+			result.data.push(item.stock)
+		})
+	}
+
+	return result
+};
